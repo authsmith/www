340c279004321dd5f26d8470a96d7cf0bdabd1d5
sourabpramanik <shubpramanik241@gmail.com>
2025-02-05 16:52:12 +0000

    feat: code ui component

---
+++ components/content/ProsePre.global.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+const props = defineProps({
+    code: {
+        type: String,
+        default: ''
+    },
+    language: {
+        type: String,
+        default: null
+    },
+    filename: {
+        type: String,
+        default: null
+    },
+    highlights: {
+        type: Array as () => number[],
+        default: () => []
+    },
+    meta: {
+        type: String,
+        default: null
+    },
+    class: {
+        type: String,
+        default: null
+    }
+})
+
+const codeCopied = ref<boolean>(false);
+
+const copyCode = (): void => {
+    navigator.clipboard.writeText(props.code)
+        .then(() => {
+            codeCopied.value = true;
+            setTimeout(function () {
+                codeCopied.value = false;
+            }, 2000);
+        })
+        .catch((e) => {
+            console.error('Error: Unable to copy code.');
+        });
+}
+</script>
+<template>
+    <div class="relative w-full">
+        <div class="flex items-center justify-between">
+            <div class="filename">
+                <span v-if="props.filename" class="leading-none">{{ filename }}</span>
+            </div>
+            <span class="text-[9px] leading-none uppercase">
+                {{ props.language }}
+            </span>
+        </div>
+        <button v-if="!codeCopied" class="absolute top-[54px] right-3" @click="copyCode">
+            <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M5 2V1H10V2H5ZM4.75 0C4.33579 0 4 0.335786 4 0.75V1H3.5C2.67157 1 2 1.67157 2 2.5V12.5C2 13.3284 2.67157 14 3.5 14H11.5C12.3284 14 13 13.3284 13 12.5V2.5C13 1.67157 12.3284 1 11.5 1H11V0.75C11 0.335786 10.6642 0 10.25 0H4.75ZM11 2V2.25C11 2.66421 10.6642 3 10.25 3H4.75C4.33579 3 4 2.66421 4 2.25V2H3.5C3.22386 2 3 2.22386 3 2.5V12.5C3 12.7761 3.22386 13 3.5 13H11.5C11.7761 13 12 12.7761 12 12.5V2.5C12 2.22386 11.7761 2 11.5 2H11Z"
+                    fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
+            </svg>
+        </button>
+        <button v-if="codeCopied" class="absolute top-[54px] right-3" @click="copyCode">
+            <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M7.49991 0.877045C3.84222 0.877045 0.877075 3.84219 0.877075 7.49988C0.877075 11.1575 3.84222 14.1227 7.49991 14.1227C11.1576 14.1227 14.1227 11.1575 14.1227 7.49988C14.1227 3.84219 11.1576 0.877045 7.49991 0.877045ZM1.82708 7.49988C1.82708 4.36686 4.36689 1.82704 7.49991 1.82704C10.6329 1.82704 13.1727 4.36686 13.1727 7.49988C13.1727 10.6329 10.6329 13.1727 7.49991 13.1727C4.36689 13.1727 1.82708 10.6329 1.82708 7.49988ZM10.1589 5.53774C10.3178 5.31191 10.2636 5.00001 10.0378 4.84109C9.81194 4.68217 9.50004 4.73642 9.34112 4.96225L6.51977 8.97154L5.35681 7.78706C5.16334 7.59002 4.84677 7.58711 4.64973 7.78058C4.45268 7.97404 4.44978 8.29061 4.64325 8.48765L6.22658 10.1003C6.33054 10.2062 6.47617 10.2604 6.62407 10.2483C6.77197 10.2363 6.90686 10.1591 6.99226 10.0377L10.1589 5.53774Z"
+                    fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
+            </svg>
+        </button>
+        <pre :class="$props.class" class="leading-none">
+            <slot/>
+        </pre>
+    </div>
+</template>
